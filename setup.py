c29b99bccfd126d0ab6e007ae3eab15876810b50
Tobias Mueller (ideabox) <tobiasmue@gnome.org>
2013-12-09 01:32:19 +0100

    Added a setup.py file to enable installation, e.g. via pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='cliresms',
+      version='0.1',
+      description='A script to send webtexts (SMS) from the command line for Irish carriers (three, O2, vodafone)',
+      url='https://github.com/russelldavies/cliresms',
+      license='Apache V2',
+      scripts=['cliresms'],
+      install_requires = [
+      ],
+      extras_require = {
+      }
+)
